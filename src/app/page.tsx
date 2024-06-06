22a12d0c3ee4fd41ccb0129b1e466aab4307f111
Walter Shewmake <waltershewmake@gmail.com>
2024-06-05 20:28:54 -0600

    feat: added logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,18 @@
-import Link from 'next/link';
+import { Center } from '@mantine/core';
+import Image from 'next/image';
+import React from 'react';
 
-import { CreatePost } from '~/app/_components/create-post';
-import styles from '~/app/index.module.css';
-import { api } from '~/trpc/server';
+export interface PageProps {}
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: 'from tRPC' });
-
+/**
+ * Home page
+ */
+const Page: React.FC<PageProps> = async ({}) => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
-        </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className={styles.showcaseContainer}>
-          <p className={styles.showcaseText}>
-            {hello ? hello.greeting : 'Loading tRPC query...'}
-          </p>
-        </div>
-
-        <CrudShowcase />
-      </div>
-    </main>
+    <Center h="80vh">
+      <Image src="/logo.png" alt="AP Peak 21 logo" fill objectFit="contain" />
+    </Center>
   );
-}
+};
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className={styles.showcaseContainer}>
-      {latestPost ? (
-        <p className={styles.showcaseText}>
-          Your most recent post: {latestPost.name}
-        </p>
-      ) : (
-        <p className={styles.showcaseText}>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}
+export default Page;
