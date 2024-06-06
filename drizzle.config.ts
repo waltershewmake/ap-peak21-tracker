9668cbed9bcd057d45547b529ded9a60d00806c7
Walter Shewmake <waltershewmake@gmail.com>
2024-06-05 19:13:30 -0600

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["ap-peak21-tracker_*"],
+} satisfies Config;
