2335f5d3a784b87ef044c7170bfacbf3d39be6ad
Walter Shewmake <waltershewmake@gmail.com>
2024-06-07 11:24:42 -0600

    fix: drizzle is kind of terrible

--- src/server/db/migrations/0003_flimsy_wolfpack.sql
+++ src/server/db/migrations/0003_flimsy_wolfpack.sql
@@ -1,2 +1,4 @@
-DROP TABLE "peak21_user";--> statement-breakpoint
-ALTER TABLE "peak21_users_to_habits" DROP CONSTRAINT "peak21_users_to_habits_user_id_peak21_user_id_fk";
+ALTER TABLE "peak21_users_to_habits"
+DROP CONSTRAINT "peak21_users_to_habits_user_id_peak21_user_id_fk";
+
+DROP TABLE "peak21_user";
