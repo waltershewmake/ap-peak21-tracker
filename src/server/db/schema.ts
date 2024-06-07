562d7b22ec2454182044c9bb40958642b92b2532
Walter Shewmake <waltershewmake@gmail.com>
2024-06-07 11:19:22 -0600

    feat: remove users table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { relations, sql } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
 import {
   date,
   pgTableCreator,
@@ -20,14 +20,6 @@ import {
  */
 export const createTable = pgTableCreator((name) => `peak21_${name}`);
 
-export const users = createTable('user', {
-  id: uuid('id').primaryKey(),
-  skoolId: varchar('skool_id', { length: 256 }).unique() /* Skool Username */,
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-});
-
 export const habits = createTable('habit', {
   id: serial('id').primaryKey(),
   name: varchar('name', { length: 256 }).notNull().unique(),
@@ -37,20 +29,10 @@ export const habits = createTable('habit', {
     .notNull(),
 });
 
-export const usersRelations = relations(users, ({ many }) => ({
-  usersToHabits: many(usersToHabits),
-}));
-
-export const habitsRelations = relations(habits, ({ many }) => ({
-  habitsToUsers: many(usersToHabits),
-}));
-
 export const usersToHabits = createTable(
   'users_to_habits',
   {
-    userId: uuid('user_id')
-      .notNull()
-      .references(() => users.id),
+    userId: uuid('user_id').notNull(),
     habitId: serial('habit_id')
       .notNull()
       .references(() => habits.id),
@@ -62,14 +44,3 @@ export const usersToHabits = createTable(
     }),
   })
 );
-
-export const usersToHabitsRelations = relations(usersToHabits, ({ one }) => ({
-  habit: one(habits, {
-    fields: [usersToHabits.habitId],
-    references: [habits.id],
-  }),
-  user: one(users, {
-    fields: [usersToHabits.userId],
-    references: [users.id],
-  }),
-}));
